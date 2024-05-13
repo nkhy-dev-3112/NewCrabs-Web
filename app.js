091b34390b388231e029fe400b0a741d581da1ad
nkhy-dev-3112 <nkhy.dev@gmail.com>
2024-05-14 05:06:02 +0700

    add: - Complete routing for admin - Move dashboard, profile of admin to view-engine

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const path = require("path");
 const port = 4000 || process.env.port;
 const expressHbs = require("express-handlebars");
 const hbs = expressHbs.create({});
+const dashboard_admin = require('./public/js/admin-dashboard-data');
 app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 expressHbs.create({
   runtimeOptions: {
@@ -25,17 +27,72 @@ app.engine(
   })
 );
 
+
 app.set("view engine", "hbs");
 
 // Register the helper
 hbs.handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
+hbs.handlebars.registerHelper('increment', function (value) {
+  return value + 1;
+});
+hbs.handlebars.registerHelper('lt', function (a, b, options) {
+  return a < b ? options.fn(this) : options.inverse(this);
+});
 
-app.get("/", (req, res)=>{
-    res.send("Hi")
+app.get("/", (req, res) => {
+  res.send("Hi")
 })
 
+// Admin routing
+app.get("/dashboard", (req, res) => {
+  res.render("admin/dashboard", {
+    layout: 'admin_layout',
+    pageName: 'Dashboard',
+    ranking_drivers: dashboard_admin.ranking_drivers,
+    cards: dashboard_admin.cards,
+    revenue_overview: dashboard_admin.revenue_overview,
+    dashboard_active: 'active'
+  })
+})
+app.get('/profile-admin', (req, res) => {
+  res.render("admin/profile", {
+    pageName: 'Profile',
+    layout: 'admin_layout',
+  })
+})
+app.get('/drivers', (req, res) => {
+  res.render("admin/drivers", {
+    pageName: 'Manage Driver',
+    layout: 'admin_layout',
+    driver_active: 'active'
+  })
+})
+app.get('/center-officers', (req, res) => {
+  res.render("admin/centre-officers", {
+    pageName: 'Manage Center Offficer',
+    layout: 'admin_layout',
+    centerofficer_active: 'active'
+  })
+})
+app.get('/vouchers', (req, res) => {
+  res.render("admin/vouchers", {
+    pageName: 'Manage Voucher',
+    layout: 'admin_layout',
+    voucher_active: 'active'
+  })
+})
+app.get('/fare-rate', (req, res) => {
+  res.render("admin/fare-rate", {
+    pageName: 'Manage Fare Rate',
+    layout: 'admin_layout',
+    farerate_active: 'active'
+  })
+})
+// Admin routing
+
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port} 🚀`);
+  console.log(`Server is running on http://localhost:${port} `);
 })
---
+++ public/js/admin-dashboard-data.js
@@ -0,0 +1,68 @@
+const dashboard_admin = {}
+
+dashboard_admin.revenue_overview = [
+    {
+        name: "Overview",
+        money: "1,233,134,245"
+    },
+    {
+        name: "In 2024",
+        money: "723,134,245"
+    },
+    {
+        name: "This month",
+        money: "233,134,245"
+    },
+    {
+        name: "Today",
+        money: "23,134,245"
+    }
+]
+dashboard_admin.ranking_drivers = [
+    {
+        name: "Nguyễn Khang Hy",
+        phone: "062 628 2434",
+        total: "23,134,245",
+        today: "1,342,463"
+    },
+    {
+        name: "Huỳnh Nguyễn Minh Huy",
+        phone: "062 628 2434",
+        total: "23,134,245",
+        today: "1,342,463"
+    },
+    {
+        name: "Nguyễn Khang Hy",
+        phone: "062 628 2434",
+        total: "23,134,245",
+        today: "1,342,463"
+    }
+]
+
+dashboard_admin.cards = [
+    {
+        title: 'Total Ride Booking',
+        ammount: '123,154',
+        rate: 12.3,
+        icon: 'fa-solid fa-money-bill'
+    },
+    {
+        title: 'Total Customer',
+        ammount: '123,154',
+        rate: 3.23,
+        icon: 'fa-solid fa-phone-volume'
+    },
+    {
+        title: 'Total Account',
+        ammount: '123,154',
+        rate: 1.24,
+        icon: 'fa-solid fa-user'
+    },
+    {
+        title: 'Total Ride Cancel',
+        ammount: '3,154',
+        rate: -0.23,
+        icon: 'fa-solid fa-xmark'
+    }
+]
+module.exports = dashboard_admin
